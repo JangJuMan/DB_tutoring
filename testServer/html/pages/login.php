443edced745db9e36049017e11beb1f5917320d3
JangJuMan <JangJuMan@github.io>
2020-05-28 23:17:47 +0900

    로그인 페이지 ~ 메인페이지 연결+ 메인페이지 페이지 분할

--- testServer/html/pages/login.php
+++ testServer/html/pages/login.php
@@ -1,7 +1,11 @@
 <html>
 <head>
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
   <link rel = "stylesheet" href = "../../css/login.css"/>
+  <style>
+    body,h1,h2,h3,h4,h5,h6, p{font-family: "Raleway", sans-serif}
+  </style>
   <script src="https://apis.google.com/js/api:client.js"></script>
   <script>
     var googleUser = {};
@@ -28,11 +32,17 @@
             + "\nGivenName : " + googleUser.getBasicProfile().getGivenName()
             + "\nFamilyName : " + googleUser.getBasicProfile().getFamilyName()
             + "\nImageUrl : " + googleUser.getBasicProfile().getImageUrl();
+            document.getElementById("form_name").value = googleUser.getBasicProfile().getName();
+            document.getElementById("form_email").value = googleUser.getBasicProfile().getEmail();
+            document.getElementById("form_img").value = googleUser.getBasicProfile().getImageUrl();
+            submit_btn.style.display = "";
+            logout_btn.style.display = "";
           },
           function(error) {
             // 경고메시지 (로그인 팝업 열었다가 닫으면 실행됨)
             // alert(JSON.stringify(error, undefined, 2));
           });
+
     }
     function signOut() {
       var auth2 = gapi.auth2.getAuthInstance();
@@ -41,21 +51,35 @@
           document.querySelector('#a').innerHTML = 'Google Login';
       });
       auth2.disconnect();
+      submit_btn.style.display = "none";
+      logout_btn.style.display = "none";
     }
   </script>
 </head>
 <body>
   <!-- In the callback, you would hide the gSignInWrapper element on a
   successful sign in -->
-  <div id="gSignInWrapper" class="login-btn">
-    <div id="customBtn" >
-        <!-- <span class="icon"></span> -->
-        <span id="a" class="customGPlusSignIn" onclick="
-        if(this.innerHTML ==='Login Successed') {signOut();} return false;
-        "> Google Login </span>
+  <div class="login-container">
+    <div class="login-title">
+      <h1>DB Tutoring Matching System</h1>
     </div>
+    <div id="gSignInWrapper" class="login-btn">
+      <div id="customBtn" >
+          <!-- <span class="icon"></span> -->
+          <span id="a" class="customGPlusSignIn" onclick="
+          if(this.innerHTML ==='Login Successed') {signOut();} return false;
+          "> Google Login </span>
+      </div>
+    </div>
+    <form action="main.php" method="post">
+      <input type="hidden" id="form_name" name="user_name" >
+      <input type="hidden" id="form_email" name="user_email" >
+      <input type="hidden" id="form_img" name="user_img" >
+      <input type="submit" id="submit_btn" value="시스템 접속하기" class="submit-btn" style="display:none">
+    </form>
+    <input type="submit" id="logout_btn" value="로그아웃" class="submit-btn" style="display:none" onclick="signOut();">
+    <div id="name"></div>
   </div>
-  <div id="name"></div>
   <script>startApp();</script>
 </body>
 </html>
