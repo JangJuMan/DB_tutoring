eac702f27998cdb26ff31c9f5dd2b4fa68d0fe83
JangJuMan <JangJuMan@github.io>
2020-05-29 17:17:35 +0900

    테스트 커밋입니다.

--- testServer/html/components/leftColumn.php
+++ testServer/html/components/leftColumn.php
@@ -8,7 +8,7 @@
      <h4 class="w3-center">My Profile</h4>
      <p class="w3-center"><img src="<?php echo $_SESSION['user_img']?>" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
      <hr>
-     <p><i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> Designer, UI</p>
+     <p><i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i><?php echo $_SESSION['user_name']?></p>
      <p><i class="fa fa-home fa-fw w3-margin-right w3-text-theme"></i> London, UK</p>
      <p><i class="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i> April 1, 1988</p>
     </div>
