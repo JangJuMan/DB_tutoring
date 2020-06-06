d47640b9ceb2e558ec85f749d062c19c5ea180ae
Wanseo JO <21700693@handong.edu>
2020-06-06 11:30:14 +0900

    testServer_wanseo

---
+++ html/DB_tutoring/testServer_wanseo/html/components/middlelookup.php
@@ -0,0 +1,34 @@
+
+<div class="w3-col m9">
+  <div class="w3-card w3-round w3-white">
+    <div class="w3-container w3-padding">
+      <div class="w3-col m12">
+        <p style= "text-align: right">
+          <button type="button" class="w3-button w3-theme write-btn">장바구니담기</button>
+          <button type="button" class="w3-button w3-theme write-btn">리뷰보기</button>
+          <h4 class="w3-center"><b>Profile</b></h4>
+        </p>
+        <p class="w3-center"><img src="<?php echo $_SESSION['user_img']?>" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
+
+        <hr class="two">
+<p style= "text-align: left">
+  <div style="padding:40px;">
+  <div class="w3-container w3-padding">
+    <div class="w3-container w3-padding">
+
+  <p><option value=1> 이름: </option></p>
+  <p><option value=1> <b>성별:</b> </option></p>
+  <p><option value=2> <b>나이:</b> </option></p>
+  <p><option value=3> <b>과외경력:</b> </option></p>
+  <p><option value=4> <b>학교:</b> </option></p>
+  <p><option value=5> <b>토플/토익 점수:</b> </option></p>
+  <p><option value=6> <b>e-mail:</b> </option></p>
+
+</div>
+  </div>
+</div>
+  </p>
+  </div>
+  </div>
+  </div>
+    </div>
